09e0c0377822b9071f0cea756de14f1172f787c7
Ivan Milchev <ivan.milchev321@gmail.com>
2021-07-07 00:17:31 +0200

    moved contact module

---
+++ routes/healthcheck/healthcheck.go
@@ -0,0 +1,20 @@
+package healthcheck
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/podkrepi-bg/module-contact/database"
+)
+
+func GetHealthcheck(db *database.Database) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		if pinger, ok := db.DB.ConnPool.(interface{ Ping() error }); ok {
+			err := pinger.Ping()
+			if err != nil {
+				return ctx.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
+					"status": fiber.StatusInternalServerError,
+				})
+			}
+		}
+		return ctx.JSON(&fiber.Map{"status": fiber.StatusOK})
+	}
+}
